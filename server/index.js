bb88bf475d61504896becaff0967349d32dbefcd
clever875 <lex.shults@gmail.com>
2025-06-23 15:28:03 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world from Express backend!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
